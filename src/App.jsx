bbdd9ed31ff0fe0bc62266185386dc66e8b1409c
H4K3R13 <arjunmundamani@gmail.com>
2024-07-13 13:14:21 +0530

    added mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./component/Home";
 
 function App() {
   return (
     <>
-      <div className="bg-white">Heloo</div>
+      <div className="bg-white">
+        <p>Aisu</p>
+      </div>
+      <Home />
     </>
   );
 }
---
+++ src/component/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import TextField from "@mui/material/TextField";
+const Home = () => {
+  return (
+    <>
+      <div className="px-1">
+        <div className="d-flex flex-column">
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
